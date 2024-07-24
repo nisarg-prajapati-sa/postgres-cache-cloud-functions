93352a8a116fa073e69a3d94e8a18c3e8a2e1815
nisarg-prajapati-sa <nisarg.prajapati@contentstack.com>
2024-07-24 17:19:38 -0400

    implement webhook

--- functions/store.js
+++ functions/store.js
@@ -1,11 +1,40 @@
 export default async function handler(req, res) {
   if (req.method == "POST") {
+    // app.js
+    const postgres = require("postgres");
+    require("dotenv").config();
+
+    let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
     try {
-      const data = req.body;
-      console.log(data);
-      res.status(200).json({ msg: data });
+      const sql = postgres({
+        host: PGHOST,
+        database: PGDATABASE,
+        username: PGUSER,
+        password: PGPASSWORD,
+        port: 5432,
+        ssl: "require",
+        connection: {
+          options: `project=${ENDPOINT_ID}`,
+        },
+      });
+
+      const payload = req.body.body;
+      const httpRequestText = JSON.stringify(req.headers);
+      const changesIn = payload.module;
+      const changesType = payload.event;
+      console.log(payload, httpRequestText, changesIn, changesType);
+      const query = `
+        INSERT INTO webhook_data (data, http_request, changes_in, changes_type)
+        VALUES ($1, $2, $3, $4)
+    `;
+      await sql`
+    INSERT INTO webhook_data (data, http_request, changes_in, changes_type)
+    VALUES (${payload}, ${httpRequestText}, ${changesIn}, ${changesType})
+  `;
+      res.status(200).send({ message: "Webhook data stored successfully" });
     } catch (error) {
-      res.status(400).json({ error: "Invalid JSON in request body" });
+      console.error("Error storing webhook data:", error);
+      res.status(500).send({ message: "Error storing webhook data" });
     }
   }
 }
