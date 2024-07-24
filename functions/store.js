218e679168791ba72c4d2829bf2501d6382202bf
nisarg-prajapati-sa <nisarg.prajapati@contentstack.com>
2024-07-24 16:03:52 -0400

    demo

--- functions/store.js
+++ functions/store.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
     try {
       const data = req.body;
       console.log(data);
+      res.status(200).json({ msg: data });
     } catch (error) {
       res.status(400).json({ error: "Invalid JSON in request body" });
     }
